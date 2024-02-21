5b8780bb0022dac8ca2a40a21e003035bea2c6de
kartguy <b.kartik3019@gmail.com>
2024-02-21 17:28:47 +0530

    edited

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const express=require("express");
 const { webRouter } = require("./routes/web");
 const app=express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 app.get('/',(req,res)=>{
     res.json({
         msg:"Hello from server"
     })
 })
-
 
 app.use('/webhook',webRouter)
 
--- server/routes/web.js
+++ server/routes/web.js
@@ -1,9 +1,36 @@
 const express=require("express");
-const { postWebHook, getWebHook } = require("../controllers/chatController");
 const router=express.Router();
 
-router.post('/webhook',postWebHook);
-router.get('/webhook',getWebHook)
+router.post('/webhook',(req,res)=>{
+    const body = req.body;
+
+    if (body.object === 'page') {
+        body.entry.forEach(entry => {
+            const webhookEvent = entry.messaging[0];
+            console.log(webhookEvent);
+            // Handle the incoming message logic here
+        });
+
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+router.get('/webhook',(req,res)=>{
+    const VERIFY_TOKEN = 'EAAXe6QCXIL4BOzetLMQrYWrrzrqa6vN4kfQXZCag1W3fX0Ooj9zZCwi4erlVPUBQ2QVQITmex5DHss0zSGyjZCYQ97i0jQxj4odSZCFLOxAAKxrpMSvzh995mZAoVlgk7EGmboQwLZAw1BsfQRkSd4iTtZC0FzpZA9KJHjdQzTEkjCiatTWDZCCJSXR0wyQvciDtu';
+    
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+
+    if (mode==='subscribe' && token === VERIFY_TOKEN) {
+
+        res.status(200).send(challenge);
+    } else {
+        res.sendStatus(403);
+    }   
+})
 
 
 module.exports={
